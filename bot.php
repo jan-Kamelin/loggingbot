2f96276a3240024b0454b58eb9dc6dc244d13038
jan-Kamelin <149723187+jan-Kamelin@users.noreply.github.com>
2023-12-20 11:59:30 -0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'isaac'
+
+config do |c|
+  c.username = "logbot"
+  c.realname = "Logging Bot"
+  c.nick    = "logbot"
+  c.server  = "irc.libera.chat"
+  c.port    = 6697
+end
+
+on :connect do
+  join "###tokipona"
+end
+
+on :channel, /.*/ do
+  open("#{channel}.log", "a") do |log|
+    log.puts "#{nick}: #{message}"
+  end
+
+  puts "#{channel}: #{nick}: #{message}"
+end
